250e4119cc86e6e7f9bd167900817cde7fb71766
Chanita-Suriyabun <158574108+Chanita-Suriyabun@users.noreply.github.com>
2024-02-02 13:47:32 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Console
+{
+    public static void Main()
+    {
+        getInt(400);
+    }
+
+    private void getInt(int args)
+    {
+        System.Console.WriteLine(args);
+    }
+}
+
+public class Console2
+{
+
+}
