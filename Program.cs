a579cb26467ab29719159748be2d7a5c59da99e4
Bo Djernis Schrøder <bdsc63266@edu.ucl.dk>
2024-02-06 19:31:20 +0100

    Bo her, jeg har tilføjet programmet til GitHub :)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Single line comment - Det kan vi bruge til at beskrive vores kode og hvad den gør.
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
