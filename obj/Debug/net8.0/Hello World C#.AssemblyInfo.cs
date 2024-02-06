25aa996883361285ad5f7638d401b100431de636
Bo Djernis Schrøder <bdsc63266@edu.ucl.dk>
2024-02-06 19:44:32 +0100

    Jeg leger med test branch

--- obj/Debug/net8.0/Hello World C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/Hello World C#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello World C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a579cb26467ab29719159748be2d7a5c59da99e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello World C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello World C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
