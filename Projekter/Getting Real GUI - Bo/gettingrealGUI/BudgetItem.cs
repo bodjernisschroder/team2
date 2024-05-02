768d6c772ca48066c912761faf0f777010faa0f5
Bo Schrøder <bdsc63266@edu.ucl.dk>
2024-05-02 11:45:49 +0200

    Bos GUI

---
+++ Projekter/Getting Real GUI - Bo/gettingrealGUI/BudgetItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal
+{
+    public class BudgetItem
+    {
+        public double Amount { get; set; }
+        public string Description { get; set; }
+
+        public BudgetItem(double amount, string description)
+        {
+            Amount = amount;
+            Description = description;
+        }
+    }
+}
---
+++ Projekter/Getting Real GUI - Bo/gettingrealGUI/NewItemPopup.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GettingReal
+{
+    public partial class NewItemPopup : Window
+    {
+
+        public NewItemPopup()
+        {
+            InitializeComponent();
+
+            string[] lines = File.ReadAllLines("C:\\Users\\bosch\\Dropbox\\PC\\Desktop\\Skole\\boprivate\\Projekter\\Getting Real UI\\gettingrealGUI\\dummydata.txt");
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+                lstSelection.Items.Add(new ListBoxItem { Content = parts[1] });
+            }
+        }
+
+        private void btnAddNewItemOK_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem selectedItem = (ListBoxItem)lstSelection.SelectedItem;
+
+            if (selectedItem == null)
+            {
+                MessageBox.Show("Vælg en ydelse fra listen for at fortsætte...");
+                return;
+            }
+
+            string description = selectedItem.Content.ToString();
+
+            ComboBoxItem selectedPriceLevel = (ComboBoxItem)cbDropdown.SelectedItem;
+            string priceLevelString = selectedPriceLevel.Content.ToString().ToLower();
+
+            double priceLevel = priceLevelString switch
+            {
+                "low" => 1100,
+                "middel" => 1350,
+                "høj" => 1600,
+            };
+
+            double timeEstimat = double.Parse(txtTimeEstimat.Text);
+            double rabat = double.Parse(txtRabat.Text) / 100;
+            double totalPrice = (timeEstimat * priceLevel) * (1 - rabat);
+
+            totalPrice = Math.Ceiling(totalPrice / 1000) * 1000;
+
+            BudgetItem newItem = new BudgetItem(totalPrice, description);
+            BudgetCreator.BudgetItems.Add(newItem);
+
+            Trace.WriteLine("OK");
+
+            DialogResult = true;
+            Close();
+        }
+        private void btnAddNewItemFortryd_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine("Fortryd");
+
+            DialogResult = false;
+            Close();
+        }
+    }
+}
