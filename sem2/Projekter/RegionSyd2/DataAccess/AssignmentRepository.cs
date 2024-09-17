417e3512395a95836afa06a714ec3480ab7a67ee
Burak Gültekin <bugl63230@edu.ucl.dk>
2024-09-17 10:41:00 +0200

    vi proever igen

---
+++ sem2/Projekter/RegionSyd2/DataAccess/AssignmentRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RegionSyd.Model;
+
+namespace RegionSyd.DataAccess
+{
+    class AssignmentRepository : IRepository<Assignment>
+    {
+        public List<Assignment> Assignments { get; set; }
+        public AssignmentRepository() 
+        {
+            Assignments = new List<Assignment>();
+        }
+        public void Add(Assignment assignment)
+        {
+            Assignments.Add(assignment);
+        }
+
+        public void Remove(Assignment assignment)
+        {
+            Assignments.Remove(assignment);
+        }
+    }
+}
---
+++ sem2/Projekter/RegionSyd2/Model/Type.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.Model
+{
+    public class Type
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime ServiceGoal { get; set; }
+
+        public Type()
+        {
+        }
+    }
+}
