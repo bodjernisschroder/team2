740a2e1420e305093163b6784758be1a9282602a
Burak Gültekin <bugl63230@edu.ucl.dk>
2024-09-17 10:43:17 +0200

    and again

---
+++ sem2/Projekter/RegionSyddanmark/Model/Region.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegionSyd.Model
+{
+    public enum Region
+    {
+        Syddanmark = 1,
+        Midtjylland,
+        Nordjylland,
+        Sjælland,
+        Hovadstaden
+    }
+}
