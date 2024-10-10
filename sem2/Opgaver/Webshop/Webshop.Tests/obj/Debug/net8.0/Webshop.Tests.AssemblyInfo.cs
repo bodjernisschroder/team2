ad60971596c20bebdb2ebd1c81f2994e77ff41de
anna <abfoldberg@gmail.com>
2024-10-10 10:10:12 +0200

    LINQ tilføjet

--- sem2/Opgaver/Webshop/Webshop.Tests/obj/Debug/net8.0/Webshop.Tests.AssemblyInfo.cs
+++ sem2/Opgaver/Webshop/Webshop.Tests/obj/Debug/net8.0/Webshop.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webshop.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704d9a4f6e2fb997a980ef5c30b2d65649606a8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b51c7e8d713736fff2da3ef9555fae3f50d88e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webshop.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webshop.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
