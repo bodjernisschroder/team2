504828809ef435bd6b589caed5b306edfc1d7972
bodjernisschroder <bdsc63266@edu.ucl.dk>
2024-12-09 19:08:18 +0100

    Bo & Burak view

--- sem2/Projekter/Publico_Kommunikation/Publico_Kommunikation.Tests/obj/Debug/net8.0-windows/Publico_Kommunikation.Tests.AssemblyInfo.cs
+++ sem2/Projekter/Publico_Kommunikation/Publico_Kommunikation.Tests/obj/Debug/net8.0-windows/Publico_Kommunikation.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Publico_Kommunikation.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efe1fff2211232f6fd79db4512b3763cbfd71f5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce60dd88b3402e44e599fa641dffd4ce4cbe6f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Publico_Kommunikation.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Publico_Kommunikation.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
